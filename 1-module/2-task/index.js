97a235b536b682b0724930032a99dffdf435c32c
MarisaSon <134845602+MarisaSon@users.noreply.github.com>
2026-05-30 15:45:09 +0300

    решила первый модуль задач

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,19 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+    if (name === null) {
+        return false;
+    }
+
+    if (name.length < 4) {
+        return false;
+    }
+
+    if (name.includes(' ')) {
+        return false;
+    }
+
+    return true;
 }
 
 function sayHello() {
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,4 @@
 function checkSpam(str) {
-  // ваш код...
+  let newStr = str.toLowerCase();
+  return newStr.includes('1xbet') || newStr.includes('xxx');
 }
