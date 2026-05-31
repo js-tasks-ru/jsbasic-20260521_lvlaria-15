c724d3872a7315bbd419656ba775d1363741beb5
MarisaSon <134845602+MarisaSon@users.noreply.github.com>
2026-05-31 16:44:02 +0300

    решила второй модуль Объекты

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,17 @@
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (Number.isFinite(salaries[key])) {
+      sum = sum + salaries[key]
+    }
+  }
+  return sum;
 }
