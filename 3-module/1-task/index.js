7ead2e25f3390cfd114ff55b7878dc6b79f805c8
MarisaSon <134845602+MarisaSon@users.noreply.github.com>
2026-06-14 16:41:54 +0300

    решила модуль 3

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,9 @@
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [vasya, petya, masha];
+
 function namify(users) {
-  // ваш код...
+  return users.map(user => user.name)
 }
--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,6 @@
+let arr = [5, 3, 8, 1];
 function filterRange(arr, a, b) {
-  // ваш код...
+  let filtered = arr.filter(item => item >= a && item <= b);
+
+  return filtered;
 }
